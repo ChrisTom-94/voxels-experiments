80b5b3bfc798e8dd5ba004df4d247d2f41acb214
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-05-25 14:21:04 +0200

    fix color issue with instancedMesh

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { Face } from 'three/examples/jsm/math/ConvexHull.js'
 
 (() => {
 
-  const MAX_VOXELS = 1000
+  const pos = new THREE.Vector3()
+  const quat = new THREE.Quaternion()
+  const scale = new THREE.Vector3()
 
   // constants
+  const MAX_VOXELS = 1000
+  const matrixShadow = new THREE.Matrix4().makeTranslation(0.5, 0.5, 0.5)
   const COLORS = [
-    0x00ff00,
-    0xff0000,
-    0x0000ff,
-    0xffff00,
-    0x00ffff,
-    0xff00ff,
+    // 0xDF1F1F, 0xDFAF1F, 0x80DF1F, 0x1FDF50, 0x1FDFDF, 0x1F4FDF, 0x7F1FDF, 0xDF1FAF, 0xEFEFEF, 0x303030
+    0x00FF00, 0xDFAF1F, 
   ];
+  const color = new THREE.Color()
 
   // scene
   const scene = new THREE.Scene()
@@ -34,77 +34,105 @@ import { Face } from 'three/examples/jsm/math/ConvexHull.js'
   // camera
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
   const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enablePan = false
+  controls.maxPolarAngle = Math.PI / 2
   camera.position.set(0, 10, 15)
 
   // lights
-  const ambientLight = new THREE.AmbientLight(0xffffff)
+  const ambientLight = new THREE.AmbientLight(0x404040);
 
   // plane
   const planeGeometry = new THREE.PlaneGeometry(20, 20)
-  const planeMaterial = new THREE.MeshBasicMaterial({color: 0x000000, side: THREE.DoubleSide, visible: false})
+  const planeMaterial = new THREE.MeshBasicMaterial({color: 0x000000, visible: false, side: THREE.DoubleSide})
   const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+  plane.name = 'plane'
   plane.rotation.x = Math.PI / 2
 
 
+  // voxels
   const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-  const cubeMaterial = new THREE.MeshToonMaterial({color: 0x00ff00})
+  const cubeMaterial = new THREE.MeshBasicMaterial()
 
-  const shadowVoxel = new THREE.Mesh(cubeGeometry, cubeMaterial)
+  const shadowVoxelMaterial = new THREE.MeshBasicMaterial({color: 0x000000, transparent: true, side: THREE.DoubleSide})
+  const shadowVoxel = new THREE.Mesh(cubeGeometry, shadowVoxelMaterial)
   shadowVoxel.material.opacity = 0.5
   const voxels = new THREE.InstancedMesh(cubeGeometry, cubeMaterial, MAX_VOXELS)
+  
+  for(let i = 0; i < MAX_VOXELS; i++) {
+    voxels.setMatrixAt(i, matrixShadow)
+    voxels.setColorAt(i, color)
+  }
+  voxels.setColorAt(0, color.setHex(0xff0000))
+  
+  voxels.instanceMatrix!.needsUpdate = true
+  voxels.instanceColor!.needsUpdate = true
+  voxels.instanceMatrix!.setUsage(THREE.DynamicDrawUsage)
+  voxels.instanceColor!.setUsage(THREE.DynamicDrawUsage)
   voxels.count = 1
+  
 
-  voxels.instanceMatrix.setUsage(THREE.DynamicDrawUsage)
-  voxels.setMatrixAt(0, new THREE.Matrix4().makeTranslation(0.5, 0.5, 0.5))
-
+  // raycaster
   const raycaster = new THREE.Raycaster()
   const mouse = new THREE.Vector2()
 
   scene.add(plane, voxels, shadowVoxel, ambientLight)
 
-  const animate = () => {
-    requestAnimationFrame(animate)
-    controls.update()
-    renderer.render(scene, camera)
+  // events
+  const onWindowResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
   }
 
   const onMouseMove = (e: MouseEvent) => {
     mouse.x = (e.clientX / window.innerWidth) * 2 - 1
-    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1
-
-    raycaster.setFromCamera(mouse, camera)
-    mouse.x = (e.clientX / window.innerWidth) * 2 - 1
-    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1
-
-    raycaster.setFromCamera(mouse, camera)
-
-    const intersects = raycaster.intersectObjects([plane, voxels])
-    
-    if(intersects.length <= 0) return
-
-    const { point } = intersects[0]
-
-    const { x, y, z } = point
-
-    shadowVoxel.position.set(
-      Math.floor(x) + 0.5,
-      Math.ceil(y) + 0.5,
-      Math.floor(z) + 0.5
-    )
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1    
   }
 
   const onClick = () => {
 
     const {x, y, z} = shadowVoxel.position
-    const matrix = new THREE.Matrix4().makeTranslation(x, y, z)
+    matrixShadow.identity().makeTranslation(x, y, z)
 
     voxels.count += 1
-    voxels.setMatrixAt(voxels.count - 1, matrix)
-    voxels.instanceMatrix.needsUpdate = true
+    voxels.setMatrixAt(voxels.count - 1, matrixShadow)
+    voxels.setColorAt(voxels.count - 1, color.setHex(COLORS[Math.floor(Math.random() * COLORS.length)]))
+    voxels.instanceMatrix!.needsUpdate = true
+    voxels.instanceColor!.needsUpdate = true
   }
 
+  window.addEventListener('resize', onWindowResize, false)
   window.addEventListener('mousemove', onMouseMove, false)
   window.addEventListener('click', onClick, false)
+
+  // render
+  const animate = () => {
+    requestAnimationFrame(animate)
+    controls.update()
+    renderer.render(scene, camera)
+
+    raycaster.setFromCamera(mouse, camera)
+
+    const intersects = raycaster.intersectObjects([plane, voxels])
+
+    if (intersects.length <= 0) {
+      shadowVoxel.visible = false
+      return
+    }
+
+    shadowVoxel.visible = true
+
+    const intersect = intersects[0]
+    const { point, face, object} = intersect
+
+    // object.matrixWorld.makeTranslation(face!.normal.x, face!.normal.y, face!.normal.z)
+    const position = new THREE.Vector3().add(point);
+    const x = Math.floor(position.x) + 0.5
+    const y = Math.floor(position.y) + 0.5 > 0.5 ? Math.floor(position.y) + 0.5 : 0.5
+    const z = Math.floor(position.z) + 0.5
+    shadowVoxel.position.set(x, y, z)
+  }
+
   animate()
 
 })();
