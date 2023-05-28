74c317187468427b88a337f93d2df86076daea80
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-05-28 18:15:25 +0200

    add selecting voxels from a range using raycast (need to be optimized)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { log } from 'three/examples/jsm/nodes/Nodes.js';
 
 (() => {
 
   // constants
 
-  const clear_btn = document.getElementById('clear') as HTMLButtonElement
-  const save_btn = document.getElementById('save') as HTMLButtonElement
-  const load_input = document.getElementById('load') as HTMLInputElement
-  const examples_select = document.getElementById('examples') as HTMLSelectElement
+  const selectionRange = document.getElementById('selection') as HTMLDivElement
+  const clearBtn = document.getElementById('clear') as HTMLButtonElement
+  const saveBtn = document.getElementById('save') as HTMLButtonElement
+  const loadInput = document.getElementById('load') as HTMLInputElement
+  const examplesSelect = document.getElementById('examples') as HTMLSelectElement
 
   const EXAMPLES = ["chicken"]
 
@@ -31,9 +33,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
   // variables
   let intersectedObjects: THREE.Intersection<THREE.Object3D<THREE.Event>>[] = []
-  let to_intersect: THREE.Object3D[] = []
-  let current_color = 0
-  let last_example = 'chicken'
+  let toIntersect: THREE.Object3D[] = []
+  let startSelection: THREE.Vector2 = new THREE.Vector2()
+  let endSelection: THREE.Vector2 = new THREE.Vector2()
+  let leftMouseDown = false
+  let currentColor = 0
+  let lastExample = 'chicken'
 
 
   // scene
@@ -82,7 +87,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
   // voxels
   const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
   
-  const shadowMaterial = new THREE.MeshBasicMaterial({color: COLORS[current_color], transparent: true, opacity: 0.5})
+  const shadowMaterial = new THREE.MeshBasicMaterial({color: COLORS[currentColor], transparent: true, opacity: 0.5})
   const shadowVoxel = new THREE.Mesh(cubeGeometry, shadowMaterial)
   shadowVoxel.material.opacity = 0.5
 
@@ -94,8 +99,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
       const json = await data.json()
 
       // clear scene
-      to_intersect.forEach((child) => scene.remove(child))
-      to_intersect = [plane]
+      toIntersect.forEach((child) => scene.remove(child))
+      toIntersect = [plane]
 
       // add voxels
       json.forEach((voxel: any) => {
@@ -104,7 +109,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
         newVoxel.position.copy(voxel.position)
         newVoxel.material.color.setHex(voxel.color)
         newVoxel.userData.isVoxel = true
-        to_intersect.push(newVoxel)
+        toIntersect.push(newVoxel)
         scene.add(newVoxel)
       })
     }
@@ -121,7 +126,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
   // add to scene
   scene.add(plane, shadowVoxel, lights)
-  to_intersect = scene.children.filter((child) => child.userData.isVoxel).concat(plane)
+  toIntersect = scene.children.filter((child) => child.userData.isVoxel).concat(plane)
 
 
   // events
@@ -138,11 +143,38 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
     mouse.x = (e.clientX / window.innerWidth) * 2 - 1
     mouse.y = -(e.clientY / window.innerHeight) * 2 + 1    
 
+    if(!(e.shiftKey && leftMouseDown)) return
+
+    endSelection.x = mouse.x
+    endSelection.y = mouse.y
+
+    const min_x = Math.min(startSelection.x, endSelection.x)
+    const max_x = Math.max(startSelection.x, endSelection.x)
+    const min_y = Math.min(startSelection.y, endSelection.y)
+    const max_y = Math.max(startSelection.y, endSelection.y)
+
+    const width = max_x - min_x
+    const height = max_y - min_y
+
+    selectionRange.style.visibility = 'visible'
+    selectionRange.style.left = `${min_x * window.innerWidth / 2 + window.innerWidth / 2}px`
+    selectionRange.style.top = `${-max_y * window.innerHeight / 2 + window.innerHeight / 2}px`
+    selectionRange.style.width = `${width * window.innerWidth / 2}px`
+    selectionRange.style.height = `${height * window.innerHeight / 2}px`
+
   }
 
   const onClick = (e: MouseEvent) => {
     
     if (e.button === 0) {
+
+      leftMouseDown = true
+
+      if (e.shiftKey) {
+        startSelection.x = mouse.x
+        startSelection.y = mouse.y
+        return
+      }
 
       if (intersectedObjects.length <= 0) return
 
@@ -150,10 +182,10 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
       const newVoxel = new THREE.Mesh(cubeGeometry, material)
 
       newVoxel.position.copy(shadowVoxel.position)
-      newVoxel.material.color.setHex(COLORS[current_color])
+      newVoxel.material.color.setHex(COLORS[currentColor])
       newVoxel.userData.isVoxel = true
   
-      to_intersect.push(newVoxel)
+      toIntersect.push(newVoxel)
       scene.add(newVoxel)
 
     } else if (e.button === 2) {
@@ -161,33 +193,75 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
       const intersect = intersectedObjects[0]
       if (!intersect || !intersect.object.userData.isVoxel) return
       
-      to_intersect.splice(to_intersect.indexOf(intersect.object), 1)
+      toIntersect.splice(toIntersect.indexOf(intersect.object), 1)
       scene.remove(intersect.object)
 
     }
 
   }
 
+  const onMouseUp = (e: MouseEvent) => {
+    console.log(leftMouseDown)
+    if (!(e.shiftKey && leftMouseDown)) return
+    leftMouseDown = false
+
+    selectionRange.style.visibility = 'hidden'
+    selectionRange.style.width = '0px'
+    selectionRange.style.height = '0px'
+
+    // throw ray for each pixel in selection range
+    const min_x = Math.min(startSelection.x, endSelection.x)
+    const max_x = Math.max(startSelection.x, endSelection.x)
+    const min_y = Math.min(startSelection.y, endSelection.y)
+    const max_y = Math.max(startSelection.y, endSelection.y)
+
+    const step = 0.01
+    let intersected = []
+    const pos = new THREE.Vector2()
+    for (let x = min_x; x < max_x; x += step) {
+      for (let y = min_y; y < max_y; y += step) {
+        pos.set(x, y)
+        raycaster.setFromCamera(pos, camera)
+        const intersects = raycaster.intersectObjects(toIntersect)
+        if (intersects.length > 0) {
+          const intersect = intersects[0]
+          if (intersect.object.userData.isVoxel) {
+            intersected.push(intersect.object)
+          }
+        }
+      }
+    }
+
+    intersected = Array.from(new Set(intersected))
+    intersected.forEach((voxel) => {
+      let mesh = voxel as THREE.Mesh
+      let material = mesh.material as THREE.MeshPhongMaterial
+      material.color.setHex(COLORS[0])
+    })
+    
+  }
+
   const onKeyDown = (e: KeyboardEvent) => {
 
-    if(e.key === 'ArrowLeft') current_color = (current_color - 1 + COLORS.length) % COLORS.length
-    if(e.key === 'ArrowRight') current_color = (current_color + 1) % COLORS.length
+    if(e.key === 'ArrowLeft') currentColor = (currentColor - 1 + COLORS.length) % COLORS.length
+    if(e.key === 'ArrowRight') currentColor = (currentColor + 1) % COLORS.length
 
-    shadowVoxel.material.color.setHex(COLORS[current_color])
+    shadowVoxel.material.color.setHex(COLORS[currentColor])
 
   }
 
   window.addEventListener('resize', onWindowResize, false)
   window.addEventListener('mousemove', onMouseMove, false)
   window.addEventListener('mousedown', onClick, false)
+  window.addEventListener('mouseup', onMouseUp, false)
   window.addEventListener('keydown', onKeyDown, false)
 
 
   const clear = () => {
       
       scene.children.filter((child) => child.userData.isVoxel).forEach((child) => scene.remove(child))
-      to_intersect = [plane]
-      last_example = ""
+      toIntersect = [plane]
+      lastExample = ""
   
   }
 
@@ -239,7 +313,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
         newVoxel.material.color.setHex(voxel.color)
         newVoxel.userData.isVoxel = true
     
-        to_intersect.push(newVoxel)
+        toIntersect.push(newVoxel)
         scene.add(newVoxel)
 
       })
@@ -260,16 +334,16 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
     if (!EXAMPLES.includes(name)) return
 
-    if (last_example === name) return
+    if (lastExample === name) return
 
     loadExample(name)
 
   }
 
-  clear_btn.addEventListener('click', clear)
-  save_btn.addEventListener('click', save)
-  load_input.addEventListener('change', load)
-  examples_select.addEventListener('change', onSelect)
+  clearBtn.addEventListener('click', clear)
+  saveBtn.addEventListener('click', save)
+  loadInput.addEventListener('change', load)
+  examplesSelect.addEventListener('change', onSelect)
 
   // render
   const animate = () => {
@@ -285,7 +359,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
     raycaster.setFromCamera(mouse, camera)
 
     intersectedObjects.length = 0
-    raycaster.intersectObjects(to_intersect, true, intersectedObjects)
+    raycaster.intersectObjects(toIntersect, true, intersectedObjects)
 
     if (intersectedObjects.length <= 0) {
       shadowVoxel.visible = false
